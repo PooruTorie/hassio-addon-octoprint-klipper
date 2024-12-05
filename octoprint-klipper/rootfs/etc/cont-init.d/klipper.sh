371a48c0ff4ba3ac9f42d1783cab857c8f7548c1
Paul Triebel <paulmagnus05@gmail.com>
2024-12-05 13:39:13 +0100

    fork

---
+++ octoprint-klipper/rootfs/etc/cont-init.d/klipper.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.info "Klipper cont-init.d"
+
+mkdir -p /data/klipper/
+
+if bashio::config.true 'update_klipper'; then
+    echo "Update Klipper"
+    cd /root/klipper && git pull origin master
+fi
+
+if [ ! -f ${HOME}/printer.cfg ]; then
+    ln -s /data/klipper/printer.cfg ${HOME}/printer.cfg
+    touch ${HOME}/printer.cfg
+fi
